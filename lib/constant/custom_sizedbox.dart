cdb7ab5ecb1aac4d962d77f0a0d20edcc19bec6e
MTS-09 <k.khushi@mirnah>
2024-11-12 10:42:32 +0530

    change font size of button

---
+++ lib/constant/custom_sizedbox.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+ SizedBox hSizedBox4 = SizedBox(width: 4.w,);
+ SizedBox hSizedBox16 = SizedBox(width: 16.w,);
+ SizedBox hSizedBox8 = SizedBox(width: 8.w,);
+ SizedBox hSizedBox32 = SizedBox(width: 32.h);
+
+ SizedBox vSizedBox4 = SizedBox(height: 4.h,);
+ SizedBox vSizedBox8 = SizedBox(height: 8.h,);
+ SizedBox vSizedBox16 = SizedBox(height:16.h,);
+ SizedBox vSizedBox32 = SizedBox(height: 32.h,);
